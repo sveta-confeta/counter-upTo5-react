354dd7868ca04f8fa067a3e1d43d630ca5621768
sveta-confeta <_svet_@tut.by>
2022-01-24 15:17:08 +0300

    count5

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,25 @@ import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./Button";
 import {Display} from "./Display";
-import Input from "./Input";
+
 
 function App() {
     let [count, setCount] = useState(0);
 
 
-    // let btn_inc=()=> count < 5 ? setCount(count + 1): count;
+    let btn_inc=()=> count < 5 ? setCount(count + 1): count;
     let btn_reset=()=> count>0 ?  setCount( count=0): count;
 
 const disabledInc= count===5;
     const disabledReset= count===0;
 
 
-    const callbackInputMax=(value:number)=>{
-         count < value ? setCount(count + 1): count;
 
-    }
 
-    const callbackInputStart=(value:number)=>{
-        count=value;
-     setCount(count+1);
-
-    }
 
 
     return (
         <div className="app_wrapper">
-        <div className="App one">
-            <div className="display_wrapper">
-                <div className="max">max value: <Input callbackInput={callbackInputMax} /></div>
-                <div className="start">start value: <Input callbackInput={callbackInputStart}/></div>
-
-            </div>
-            <div className="btn_wrapper_one">
-                <Button name='set' callback={} disabled={disabledInc}/>
-
-            </div>
-        </div>
             <div className="App two">
                 <div className="display_wrapper">
                     <Display count={count}/>
